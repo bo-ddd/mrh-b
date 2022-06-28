55796cf2a600bc60e3c404ae5f13d09400e77f93
wbl <1055079472@qq.com>
2022-06-28 09:44:10 +0800

    完成home页面，开始商品管理页面

---
+++ src/view/home/Commodity.vue
@@ -0,0 +1,22 @@
+<template>
+  <div class="box">
+    <div class="title">
+      <span>商品名称</span>
+      <el-input v-model="input" placeholder="Please input" clearable />
+    </div>
+  </div>
+</template>
+
+<script>
+export default {};
+</script>
+
+<style scoped>
+.box {
+  height: calc(100vh - 100px);
+  box-sizing: border-box;
+  background-color: rgba(233, 233, 233, 0.761);
+  border-radius: 5px;
+  padding: 20px;
+}
+</style>
